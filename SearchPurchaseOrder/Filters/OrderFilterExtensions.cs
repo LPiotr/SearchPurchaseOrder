c09c257255eac98197b7400002fe7bafed3e72ab
LPiotr1 <piotr.lebelt1@gmail.com>
2023-08-23 22:20:39 +0200

    użycie Linq w klasie OrderFilterExtensions

--- SearchPurchaseOrder/Filters/OrderFilterExtensions.cs
+++ SearchPurchaseOrder/Filters/OrderFilterExtensions.cs
@@ -7,27 +7,26 @@ namespace SearchPurchaseOrder.Filters
     {
         public static IEnumerable<PurchaseOrder> FilterByNumber(this IEnumerable<PurchaseOrder> orders, string? orderNumber)
         {
-            return string.IsNullOrEmpty(orderNumber) ? orders : orders.Where(o => o.Number == orderNumber);
+            return string.IsNullOrEmpty(orderNumber)
+                   ? orders
+                   : orders.Where(o => o.Number == orderNumber);
         }
 
         public static IEnumerable<PurchaseOrder> FilterByClientCode(this IEnumerable<PurchaseOrder> orders, List<string>? clientCodes)
         {
-            return clientCodes?.Count > 0 ? orders.Where(o => clientCodes.Contains(o.ClientCode)) : orders;
+            return (clientCodes == null || !clientCodes.Any())
+                   ? orders
+                   : orders.Where(o => clientCodes.Contains(o.ClientCode));
         }
 
         public static IEnumerable<PurchaseOrder> FilterByDate(this IEnumerable<PurchaseOrder> orders, string? startDate, string? endDate)
         {
-            if (!string.IsNullOrEmpty(startDate))
-            {
-                DateTime parsedStartDate = DateTime.ParseExact(startDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-                orders = orders.Where(o => o.OrderDate >= parsedStartDate);
-            }
-            if (!string.IsNullOrEmpty(endDate))
-            {
-                DateTime parsedEndDate = DateTime.ParseExact(endDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-                orders = orders.Where(o => o.OrderDate <= parsedEndDate);
-            }
-            return orders;
+            var culture = CultureInfo.InvariantCulture;
+            bool isStartDateValid = DateTime.TryParseExact(startDate, "dd.MM.yyyy", culture, DateTimeStyles.None, out DateTime startDateTime);
+            bool isEndDateValid = DateTime.TryParseExact(endDate, "dd.MM.yyyy", culture, DateTimeStyles.None, out DateTime endDateTime);
+
+            return orders.Where(o => (!isStartDateValid || o.OrderDate >= startDateTime) &&
+                                     (!isEndDateValid || o.OrderDate <= endDateTime));
         }
     }
 }

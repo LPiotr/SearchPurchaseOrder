2be6807aa7ad44bc874fa5fd759a37646746a766
LPiotr1 <piotr.lebelt1@gmail.com>
2023-08-30 12:15:06 +0200

    Fixed csv class

--- SearchPurchaseOrder/Services/CsvOrderFileReader.cs
+++ SearchPurchaseOrder/Services/CsvOrderFileReader.cs
@@ -9,15 +9,18 @@ namespace SearchPurchaseOrder.Interfaces
     public sealed class CsvOrderFileReader : IPurchaseOrderFileReader
     {
         private static readonly CsvOrderFileReader _instance = new();
-        private readonly List<PurchaseOrder> _orders = new();
+        private List<PurchaseOrder>? _orders = new();
 
-        private CsvOrderFileReader() { }
+        private CsvOrderFileReader() 
+        {
+            _orders = null; 
+        }
 
         public static CsvOrderFileReader Instance => _instance;
 
         public async Task<IEnumerable<PurchaseOrder>> ReadOrders(string path)
         {
-            if (_orders.Any()) { return _orders; }
+            if (_orders != null) { return _orders; }
 
             var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
@@ -30,15 +33,18 @@ namespace SearchPurchaseOrder.Interfaces
             using var csv = new CsvReader(reader, config);
             csv.Context.RegisterClassMap<PurchaseOrderMap>();
 
+            var orders = new List<PurchaseOrder>();
+
             while (await csv.ReadAsync())
             {
                 var order = csv.GetRecord<PurchaseOrder>();
                 if (order != null)
                 {
-                    _orders.Add(order);
+                    orders.Add(order);
                 }
             }
 
+            _orders = orders;
             return _orders;
         }
     }
